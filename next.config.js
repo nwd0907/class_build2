de49d21395ec49ba1e7b152fc0d415666c0e4aa3
nwd0907 <nwd0907@newbizstart.com>
2023-05-04 11:11:38 +0900

    빌드아이디 통합

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   generateBuildId: () => "codecamp-deploy-project",
 
+  // 아래 주소들만 out폴더에 만들어줘! => getServerSideProps 있는 페이지는 제외
   exportPathMap: () => {
     return {
       "/": { page: "/" },
